43414fb1ed74f9f17106e2cf70a9ec2597f59aa9
Alex <alexandrazaitzewa@yandex.ru>
2021-10-10 09:00:15 +0500

    Simple web page input

---
+++ Application/Application/Controllers/MainPageController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    public class MainPageController : Controller
+    {
+        public ActionResult Index()  => View();
+        
+        [HttpPost]
+        public string GetHoursInfo(List<StudyHoursInfo> model) => "Oтправлено";
+        
+    }
+}
---
+++ Application/Application/Models/StudyHoursInfo.cs
@@ -0,0 +1,11 @@
+namespace Application
+{
+    public class StudyHoursInfo
+    {
+        //[Required]
+        public string Group { get; set; }
+        public string Teacher { get; set; }
+        public string Subject { get; set; }
+        public int Hours { get; set; }
+    }
+}
