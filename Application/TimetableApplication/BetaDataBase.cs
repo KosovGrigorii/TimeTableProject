0bb5301a6f71c4c51b7dcd0711d48a9c5a7fa299
KrauhinSemen <72562026+KrauhinSemen@users.noreply.github.com>
2021-11-21 12:27:06 +0500

    initial-option-for-parsing-input

---
+++ Application/TimetableApplication/BetaDataBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TimetableDomain;
+
+namespace TimetableApplication
+{
+    public static class DataBase
+    {
+        private static List<Course> courses = new List<Course>();
+        private static List<Group> groups = new List<Group>();
+        private static List<Class> classes = new List<Class>();
+        private static List<Teacher> teachers = new List<Teacher>();
+
+        public static void AddCourse(Course _course)
+        {
+            courses.Add(_course);
+        }
+
+        public static void AddGroup(Group _group)
+        {
+            groups.Add(_group);
+        }
+
+        public static void AddClass(Class _class)
+        {
+            classes.Add(_class);
+        }
+
+        public static void AddTeacher(Teacher _teacher)
+        {
+            teachers.Add(_teacher);
+        }
+    }
+}
---
+++ Application/TimetableApplication/SortClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TimetableDomain;
+
+namespace TimetableApplication
+{
+    public class SortClass
+    {
+        public static void Sort(List<SlotInfo> tableInfo)
+        {
+            foreach (var slotInfo in tableInfo)
+            {
+                if (slotInfo.Course != null) DataBase.AddCourse(ParseSlotCourse(slotInfo.Course));
+                if (slotInfo.Group != null) DataBase.AddGroup(ParseSlotGroup(slotInfo.Group));
+                if (slotInfo.Class != null) DataBase.AddClass(ParseSlotClass(slotInfo.Group));
+                if (slotInfo.Teacher != null) DataBase.AddTeacher(ParseSlotTeacher(slotInfo.Group));
+            }
+        }
+        //"{\"Name\":\"Tom\",\"Age\":35}"
+        //Над форматом обработки ведутся работы
+        private static Course ParseSlotCourse(string _course)
+        {
+            return new Course();
+        }
+
+        private static Group ParseSlotGroup(string _group)
+        {
+            return new Group();
+        }
+
+        private static Class ParseSlotClass(string _class)
+        {
+            return new Class();
+        }
+
+        private static Teacher ParseSlotTeacher(string _teacher)
+        {
+            return new Teacher();
+        }
+    }
+}
