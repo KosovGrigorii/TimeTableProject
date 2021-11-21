407f600f78e6a143fdcd49e68a61df7567d3dafa
Alexandra Zaytseva <alexandrazaitzewa@yandex.ru>
2021-11-21 11:12:29 +0500

    Input and .xlsx parser

--- Application/TimetableApplication/XlsxInputParser.cs
+++ Application/TimetableApplication/XlsxInputParser.cs
@@ -1,12 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using ExcelDataReader;
 using Infrastructure;
 
 namespace TimetableApplication
 {
     public class XlsxInputParser : IInputParser
     {
-        public void ParseFile()
+        public IEnumerable<SlotInfo> ParseFile(MemoryStream stream)
         {
-            throw new System.NotImplementedException();
+            var slots = new List<SlotInfo>();
+            stream.Position = 0;
+            using var reader = ExcelReaderFactory.CreateReader(stream);
+            try
+            {
+                while (reader.Read())
+                {
+                    slots.Add(new SlotInfo{
+                        Course = reader.GetValue(0).ToString(), 
+                        Group = reader.GetValue(1).ToString(), 
+                        Teacher = reader.GetValue(2).ToString(),
+                        Class = reader.GetValue(3).ToString()});
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new ArgumentException(".xlsx file was filled out wrongly");
+            }
+
+            return slots;
         }
     }
 }
