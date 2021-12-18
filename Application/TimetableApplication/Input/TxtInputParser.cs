1d6ef7532de7020810bcc484fddc5bbc89bfcefa
KrauhinSemen <72562026+KrauhinSemen@users.noreply.github.com>
2021-12-18 16:45:11 +0500

    TxtInputParser

---
+++ Application/TimetableApplication/Input/TxtInputParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TimetableApplication.Input
+{
+    class TxtInputParser
+    {
+        public string Extension => ".txt";
+
+        public IEnumerable<SlotInfo> ParseFile(Stream stream)
+        {
+            var slots = new List<SlotInfo>();
+            stream.Position = 0;
+            using var reader = new StreamReader(stream);
+            try
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    var slot = line.Split('|');
+                    slots.Add(new SlotInfo
+                    {
+                        Class = slot[0].ToString(),
+                        Course = slot[1].ToString(),
+                        Group = slot[2].ToString(),
+                        Teacher = slot[3].ToString()
+                    });
+                    line = reader.ReadLine();
+                }
+                // reader.Close(); Нужно ли закрывать?
+            }
+            catch (NullReferenceException)
+            {
+                throw new ArgumentException(".xlsx file was filled out wrongly");
+            }
+            return slots;
+        }
+    }
+}
