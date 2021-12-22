4f9000516b823a68e9df79783aac42a87cfee5f2
KrauhinSemen <72562026+KrauhinSemen@users.noreply.github.com>
2021-12-22 17:56:56 +0500

    fixed inputParser

--- Application/UserInterface/Input/TxtInputParser.cs
+++ Application/UserInterface/Input/TxtInputParser.cs
@@ -1,23 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using TimetableApplication;
+using UserInterface;
 
-namespace UserInterface
+namespace TimetableApplication.Input
 {
     class TxtInputParser : IInputParser
     {
+        public (double, double) GetSpan(string begin, string end)
+        {
+            var temp = begin.Split();
+            begin = temp[temp.Length - 1];
+            temp = end.Split();
+            end = temp[temp.Length - 1];
+            temp = begin.Split(':');
+            var begin_span = new TimeSpan(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]));
+            temp = end.Split(':');
+            var end_span = new TimeSpan(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]));
+            return (begin_span.TotalMinutes, end_span.TotalMinutes);
+        }
+
         public ParserExtension Extension => ParserExtension.Txt;
 
         public IEnumerable<SlotInfo> ParseFile(Stream stream)
         {
             var slots = new List<SlotInfo>();
+            var starts = new List<TimeSpan>();
+            var duration = 90;
             stream.Position = 0;
             using var reader = new StreamReader(stream);
             try
             {
-                string line = reader.ReadLine();
-                while (line != null)
+                var line = reader.ReadLine();
+                while (line != "-")
                 {
                     var slot = line.Split('|');
                     slots.Add(new SlotInfo
@@ -29,13 +44,44 @@ namespace UserInterface
                     });
                     line = reader.ReadLine();
                 }
-                // reader.Close(); Нужно ли закрывать?
+                line = reader.ReadLine();
+                var info = line.Split();
+                var (begin, end) = GetSpan(info[0], info[1]);
+                duration = int.Parse(info[2]);
+                var rest = int.Parse(info[3]);
+                var special_rest = new List<double>();
+                for (var i = 4; i < info.Length; i += 2)
+                {
+                    var (start_rest, end_rest) = GetSpan(info[i], info[i + 1]);
+                    special_rest.Add(start_rest);
+                    special_rest.Add(end_rest);
+                }
+                while (begin < end)
+                {
+                    if (special_rest.Count == 0 || begin + duration < special_rest[0])
+                    {
+                        starts.Add(TimeSpan.FromMinutes(begin));
+                        begin += duration + rest;
+                    }
+                    else
+                    {
+                        starts.Add(TimeSpan.FromMinutes(begin));
+                        begin = special_rest[1];
+                        special_rest.Remove(special_rest[0]);
+                        special_rest.Remove(special_rest[0]);
+                    }
+                }
+                reader.Close();
             }
             catch (NullReferenceException)
             {
                 throw new ArgumentException(".xlsx file was filled out wrongly");
             }
+            foreach (var i in starts)
+                Console.WriteLine(i);
+            Console.WriteLine(duration);
             return slots;
+            //return (slots, starts, duration);
         }
     }
 }
--- Application/UserInterface/Input/XlsxInputParser.cs
+++ Application/UserInterface/Input/XlsxInputParser.cs
@@ -1,38 +1,84 @@
 using System;
 using System.Collections.Generic;
-using TimetableApplication;
 using System.IO;
 using ExcelDataReader;
+using UserInterface;
 
-namespace UserInterface
+namespace TimetableApplication
 {
     public class XlsxInputParser : IInputParser
     {
+        public (double, double) GetSpan(string begin, string end)
+        {
+            var temp = begin.Split();
+            begin = temp[temp.Length - 1];
+            temp = end.Split();
+            end = temp[temp.Length - 1];
+            temp = begin.Split(':');
+            var begin_span = new TimeSpan(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]));
+            temp = end.Split(':');
+            var end_span = new TimeSpan(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]));
+            return (begin_span.TotalMinutes, end_span.TotalMinutes);
+        }
+
         public ParserExtension Extension => ParserExtension.Xlsx;
-        
+
         public IEnumerable<SlotInfo> ParseFile(Stream stream)
         {
-            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
             var slots = new List<SlotInfo>();
+            var starts = new List<TimeSpan>();
+            var duration = 90;
             stream.Position = 0;
             using var reader = ExcelReaderFactory.CreateReader(stream);
             try
             {
                 while (reader.Read())
                 {
-                    slots.Add(new SlotInfo{
-                        Course = reader.GetValue(0).ToString(), 
-                        Group = reader.GetValue(1).ToString(), 
+                    if (reader.GetValue(0).ToString() == "-") break;
+                    slots.Add(new SlotInfo
+                    {
+                        Course = reader.GetValue(0).ToString(),
+                        Group = reader.GetValue(1).ToString(),
                         Teacher = reader.GetValue(2).ToString(),
-                        Class = reader.GetValue(3).ToString()});
+                        Class = reader.GetValue(3).ToString()
+                    });
                 }
+                reader.Read();
+                var (begin, end) = GetSpan(reader.GetValue(0).ToString(), reader.GetValue(1).ToString());
+                duration = int.Parse(reader.GetValue(2).ToString());
+                var rest = int.Parse(reader.GetValue(3).ToString());
+                var special_rest = new List<double>();
+                var temp = 4;
+                while (reader.GetValue(temp) != null)
+                {
+                    var (start_rest, end_rest) = GetSpan(reader.GetValue(temp).ToString(), reader.GetValue(temp + 1).ToString());
+                    special_rest.Add(start_rest);
+                    special_rest.Add(end_rest);
+                    temp += 2;
+                }
+                while (begin < end)
+                {
+                    if (special_rest.Count == 0 || begin + duration < special_rest[0])
+                    {
+                        starts.Add(TimeSpan.FromMinutes(begin));
+                        begin += duration + rest;
+                    }
+                    else
+                    {
+                        starts.Add(TimeSpan.FromMinutes(begin));
+                        begin = special_rest[1];
+                        special_rest.Remove(special_rest[0]);
+                        special_rest.Remove(special_rest[0]);
+                    }
+                }
+                reader.Close();
             }
             catch (NullReferenceException)
             {
                 throw new ArgumentException(".xlsx file was filled out wrongly");
             }
-
             return slots;
+            //return (slots, starts, duration);
         }
     }
 }
