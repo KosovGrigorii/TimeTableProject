c348497e9e656b38b85956446ac80c36499f9d27
Alexandra Zaytseva <alexandrazaitzewa@yandex.ru>
2021-12-24 14:27:10 +0500

    Added week day filters input

--- Application/UserInterface/Controllers/FiltersInputController.cs
+++ Application/UserInterface/Controllers/FiltersInputController.cs
@@ -32,20 +32,28 @@ namespace UserInterface
 
         public PartialViewResult ChooseSingleFilter(string filterName, string userId, string elementId)
         {
-            return _SingleSpecifiedFilter(userId, elementId);
+            var specifiedFilters = databaseProvider.GetTeacherFilters(userId);
+            if (filterName == "Working days amount")
+                return GetWorkingDaysCountFilter(specifiedFilters, elementId);
+            return GetSpecifiedWorkingDaysFilter(specifiedFilters, elementId);
         }
         
-        public PartialViewResult _SingleSpecifiedFilter(string uid, string elementId)
+        public PartialViewResult GetWorkingDaysCountFilter(IEnumerable<string> specifiedFilters, string elementId)
         {
-            var specifiedFilters = databaseProvider.GetTeacherFilters(uid);
             var userFilters = new UserFilters() {Filters = specifiedFilters, Index = elementId};
-            return PartialView(userFilters);
+            return PartialView("_DaysCountFilter", userFilters);
+        }
+
+        public PartialViewResult GetSpecifiedWorkingDaysFilter(IEnumerable<string> specifiedFilters, string elementId)
+        {
+            var weekDayFilters = new UserWeekdayFilters() {Filters = specifiedFilters, Index = elementId};
+            return PartialView("_DaysFilter", weekDayFilters);
         }
         
         [HttpPost]
         public IActionResult GetFilters(IEnumerable<FilterUI> filters, string uid)
         {
-            var applicationFilters = filters.Select(x => new Filter(x.Name, x.Days));
+            var applicationFilters = filters.Select(x => new Filter(x.Name, x.DaysCount, x.Days));
 
             timetableMaker.StartMakingTimeTable(uid, databaseProvider, applicationFilters);
             var timetableTask = new Task(() => timetableMaker.StartMakingTimeTable(uid, databaseProvider, applicationFilters));
