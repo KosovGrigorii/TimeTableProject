ca5c715412346f0a201b44b4934ef4d7b3f80c5f
KrauhinSemen <72562026+KrauhinSemen@users.noreply.github.com>
2021-12-25 17:14:09 +0500

    update input

---
+++ Application/TimetableApplication/Times.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace TimetableApplication
+{
+    public class Times
+    {
+        public List<Tuple<TimeSpan, int>> times { get; set; }
+    }
+}
